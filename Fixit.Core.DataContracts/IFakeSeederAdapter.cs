8aacf62f721ac0681d53094f6aec746742e18218
Ahmed Popal <ahmed@empower.social>
2020-12-06 13:47:47 -0500

    Added two adapters for data contract fake seeding

---
+++ Fixit.Core.DataContracts/IFakeSeederAdapter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Empower.Core.DataContracts.Adapters
+{
+  public interface IFakeSeederAdapter<T> where T : class
+  {
+    IList<T> SeedFakeDtos();
+  }
+}
---
+++ Fixit.Core.DataContracts/IFakeSeederFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace Empower.Core.DataContracts.Adapters
+{
+  public interface IFakeSeederFactory
+  {
+    IFakeSeederAdapter<T> CreateFakeSeeder<T>() where T : class;
+  }
+}
