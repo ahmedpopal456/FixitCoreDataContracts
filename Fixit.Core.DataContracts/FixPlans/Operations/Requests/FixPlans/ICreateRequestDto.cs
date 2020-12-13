956bf6ca2db636ce85ea21e39ef62a964d85eaeb
Weigiu <35940338+Weigiu@users.noreply.github.com>
2020-12-13 16:58:44 -0500

    create request dto

---
+++ Fixit.Core.DataContracts/FixPlans/Operations/Requests/FixPlans/ICreateRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fixit.Core.DataContracts.FixPlans.Operations.Requests.FixPlans
+{
+  public interface ICreateRequestDto
+  {
+  }
+}
