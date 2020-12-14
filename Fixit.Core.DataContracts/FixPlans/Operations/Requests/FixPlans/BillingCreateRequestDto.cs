27d130b652087cc55bfd733f265dba5ca800506f
Weigiu <35940338+Weigiu@users.noreply.github.com>
2020-12-14 10:32:57 -0500

    add billing and phase

---
+++ Fixit.Core.DataContracts/FixPlans/Operations/Requests/FixPlans/BillingCreateRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fixit.Core.DataContracts.FixPlans.BillingDetails.Enums;
+using Fixit.Core.DataContracts.FixPlans.Phases.Cost;
+
+namespace Fixit.Core.DataContracts.FixPlans.Operations.Requests.FixPlans
+{
+  public class BillingCreateRequestDto : ICreateRequestDto
+  {
+    public float InitialCost { get; set; }
+  }
+}
---
+++ Fixit.Core.DataContracts/FixPlans/Operations/Requests/FixPlans/FixPhaseCreateRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fixit.Core.DataContracts.FixPlans.Operations.Requests.FixPlans
+{
+  class FixPhaseCreateRequestDto : ICreateRequestDto
+  {
+    public string Name { get; set; }
+  }
+}
