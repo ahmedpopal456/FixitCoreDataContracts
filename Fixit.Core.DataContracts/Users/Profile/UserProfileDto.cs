270e25b0d75ef92a3a05388399eb6e10f41574b8
Joanna Lin <33985435+jayohanna@users.noreply.github.com>
2020-12-23 14:37:51 -0500

    UserProfileDto inherit from UserProfileInformationDto

--- Fixit.Core.DataContracts/Users/Profile/UserProfileDto.cs
+++ Fixit.Core.DataContracts/Users/Profile/UserProfileDto.cs
@@ -5,18 +5,9 @@ using Fixit.Core.DataContracts.Users.Address;
 namespace Fixit.Core.DataContracts.Users.Profile
 {
   [DataContract]
-  public class UserProfileDto : OperationStatus
+  public class UserProfileDto : UserProfileInformationDto
   {
     [DataMember]
     public string ProfilePictureUrl { get; set; }
-
-    [DataMember]
-    public string FirstName { get; set; }
-
-    [DataMember]
-    public string LastName { get; set; }
-
-    [DataMember]
-    public AddressDto Address { get; set; }
   }
 }
