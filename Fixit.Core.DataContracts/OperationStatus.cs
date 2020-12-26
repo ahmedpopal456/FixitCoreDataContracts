0b9616d965a7e4e4935a4717d7e116164ec68185
Paul Musat <pmusat@outlook.com>
2020-12-26 13:11:48 -0500

    (#267) Moved OperationStatus from Fixit.Core.Database into current repo.

---
+++ Fixit.Core.DataContracts/OperationStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fixit.Core.DataContracts
+{
+  // TODO: add Fixit error code definitions
+  public class OperationStatus
+  {
+    public bool IsOperationSuccessful { get; set; }
+
+    public string OperationMessage { get; set; }
+
+#nullable enable
+    public Exception? OperationException { get; set; }
+#nullable disable
+  }
+}
