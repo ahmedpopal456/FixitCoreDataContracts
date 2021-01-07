a195b069b8532ac193e30f53040b92316b08c5e5
Weigiu <35940338+Weigiu@users.noreply.github.com>
2021-01-07 10:23:57 -0500

    update request dtos

--- Fixit.Core.DataContracts/FixPlans/Operations/Requests/FixPlans/FixPlanCreateRequestDto.cs
+++ Fixit.Core.DataContracts/FixPlans/Operations/Requests/FixPlans/FixPlanCreateRequestDto.cs
@@ -1,29 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Fixit.Core.DataContracts.FixPlans.BillingDetails;
-using Fixit.Core.DataContracts.FixPlans.Enums;
 using Fixit.Core.DataContracts.FixPlans.Phases;
 using Fixit.Core.DataContracts.Users;
 
 namespace Fixit.Core.DataContracts.FixPlans.Operations.Requests.FixPlans
 {
-  public class FixPlanRequestDto
+  //change to create requestDto
+  public class FixPlanCreateRequestDto
   {
     public Guid FixId { get; set; }
 
     public bool IsBookmarked { get; set; }
 
-    public FixPlanProposalStates ProposalState { get; set; }
-
+    
     public float TotalCost { get; set; }
 
-    public Guid ActivePhaseId { get; set; }
-
+    
     public FixPlanBillingDetailsDto BillingDetails { get; set; }
 
     public IEnumerable<FixPhaseDto> Phases { get; set; }
 
     public UserSummaryDto CreatedByCraftsman { get; set; }
+
+    //public FixPlanProposalStates ProposalState { get; set; }
+    //public Guid ActivePhaseId { get; set; }
   }
 }
---
+++ Fixit.Core.DataContracts/FixPlans/Operations/Requests/FixPlans/FixPlanUpdateRequestDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fixit.Core.DataContracts.FixPlans.Phases;
+
+namespace Fixit.Core.DataContracts.FixPlans.Operations.Requests.FixPlans
+{
+  public class FixPlanUpdateRequestDto
+  {
+      public bool IsBookmarked { get; set; }
+
+      public IEnumerable<FixPhaseDto> Phases { get; set; }
+
+      //public FixPlanProposalStates ProposalState { get; set; }
+
+      //public Guid ActivePhaseId { get; set; }
+
+      //public Guid FixId { get; set; }
+  }
+}
