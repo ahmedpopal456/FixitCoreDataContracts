575048927fd49cbd518c21865e0e178842324929
Weigiu <35940338+Weigiu@users.noreply.github.com>
2021-01-09 14:42:17 -0500

    remove unused using

--- Fixit.Core.DataContracts/FixPlans/Operations/Requests/FixPlanUpdateRequestDto.cs
+++ Fixit.Core.DataContracts/FixPlans/Operations/Requests/FixPlanUpdateRequestDto.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Runtime.Serialization;
-using System.Text;
 using Fixit.Core.DataContracts.FixPlans.Phases;
 
 namespace Fixit.Core.DataContracts.FixPlans.Operations.Requests.FixPlans
