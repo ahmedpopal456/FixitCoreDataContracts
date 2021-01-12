b51402785d4b3f4b9f4f8ad00fcdd9f24da314ff
Paul Musat <pmusat@outlook.com>
2021-01-12 00:39:46 +0000

    Merged PR 75: (#257) User Account Operation Dtos
    Related work items: #257

---
+++ Fixit.Core.DataContracts/Users/Account/UserAccountStateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+using Fixit.Core.DataContracts;
+using Fixit.Core.DataContracts.Users.Enums;
+
+namespace Fixit.Core.DataContracts.Users.Account
+{
+  [DataContract]
+  public class UserAccountStateDto : OperationStatus
+  {
+    [DataMember]
+    public UserState State { get; set; }
+  }
+}
---
+++ Fixit.Core.DataContracts/Users/Operations/Account/UserAccountRoleResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+using Fixit.Core.DataContracts;
+using Fixit.Core.DataContracts.Users.Enums;
+
+namespace Fixit.Core.DataContracts.Users.Operations.Account
+{
+  [DataContract]
+  public class UserAccountRoleResponseDto : OperationStatus
+  {
+    [DataMember]
+    public string UserId { get; set; }
+
+    [DataMember]
+    public UserRole Role { get; set; }
+  }
+}
