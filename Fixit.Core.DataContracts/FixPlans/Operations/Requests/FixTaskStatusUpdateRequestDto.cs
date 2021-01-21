4d3c2aa005b9a0fd0bf2268466e577291d4998d7
Wei-Cheng Su <wc.su598@gmail.com>
2021-01-21 00:22:53 +0000

    Merged PR 80: Fix Plan request Dtos
    Fix Plan, Fix Phase and Fix Task request dtos
    
    Related work items: #260

---
+++ Fixit.Core.DataContracts/FixPlans/Operations/Requests/FixTaskStatusUpdateRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+using Fixit.Core.DataContracts.FixPlans.Phases.Tasks.Enums;
+
+namespace Fixit.Core.DataContracts.FixPlans.Operations.Requests.FixPlans
+{
+  [DataContract]
+  public class FixTaskStatusUpdateRequestDto
+  {
+    [DataMember]
+    public TaskStatuses Status { get; set; }
+  }
+}
