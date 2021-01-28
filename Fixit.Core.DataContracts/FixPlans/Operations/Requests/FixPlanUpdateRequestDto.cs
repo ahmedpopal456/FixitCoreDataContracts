6e433a922490af130f39726a8db42dc5632f5738
Paul Musat <pmusat@outlook.com>
2021-01-28 01:23:33 +0000

    Merged PR 121: (#188, #256, #257, #260, #290, #368) Merge staging to master
    Related work items: #188, #256, #257, #260, #267, #290, #304, #309, #344, #368

---
+++ Fixit.Core.DataContracts/FixPlans/Operations/Requests/FixPlanUpdateRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Fixit.Core.DataContracts.FixPlans.Phases;
+
+namespace Fixit.Core.DataContracts.FixPlans.Operations.Requests.FixPlans
+{
+  [DataContract]
+  public class FixPlanUpdateRequestDto
+  {
+      [DataMember]
+      public bool IsBookmarked { get; set; }
+      
+      [DataMember]
+      public IEnumerable<FixPhaseDto> Phases { get; set; }
+
+      [DataMember]
+      public long UpdatedTimestampUtc { get; set; }
+  }
+}
