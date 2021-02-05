ca9b0b0d7dce2cc8526877c055963771ca8389be
David Chen <DavidChen_98@hotmail.com>
2021-02-05 00:47:40 +0000

    Merged PR 107: #259 Fix route request Dtos
    Added
    - FixCreateRequestDto
    - FixRequestDto
    - FixUpdateAssignRequestDto
    - FixUpdateDto
    - FixGetCostResponseDto
    
    Related work items: #259

---
+++ Fixit.Core.DataContracts/Fixes/Operations/Requests/FixUpdateRequestDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Fixit.Core.DataContracts.Fixes.Details;
+using Fixit.Core.DataContracts.Fixes.Enums;
+using Fixit.Core.DataContracts.Fixes.Files;
+using Fixit.Core.DataContracts.Fixes.Locations;
+using Fixit.Core.DataContracts.Fixes.Schedule;
+using Fixit.Core.DataContracts.Fixes.Tags;
+using Fixit.Core.DataContracts.Users;
+
+namespace Fixit.Core.DataContracts.Fixes.Operations.Requests
+{
+  [DataContract]
+  public class FixUpdateRequestDto : OperationStatus
+  {
+
+    [DataMember]
+    public IEnumerable<TagDto> Tags { get; set; }
+
+    [DataMember]
+    public IEnumerable<FixDetailsDto> Details { get; set; }
+
+    [DataMember]
+    public IEnumerable<FileDto> Images { get; set; }
+
+    [DataMember]
+    public FixLocationDto Location { get; set; }
+
+    [DataMember]
+    public IEnumerable<FixScheduleRangeDto> Schedule { get; set; }
+
+    [DataMember]
+    public UserSummaryDto UpdatedByUser { get; set; }
+
+    [DataMember]
+    public FixStatuses Status { get; set; }
+
+  }
+}
