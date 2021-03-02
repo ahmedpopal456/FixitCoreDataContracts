b04a4605e616507110c63135cacb675de95242dc
Wei-Cheng Su <wc.su598@gmail.com>
2021-03-02 02:29:25 +0000

    Merged PR 202: add new seeders for FMS
    Related work items: #259, #369

--- Fixit.Core.DataContracts/FixPlans/Operations/Requests/FixPhaseStatusUpdateRequestDto.cs
+++ Fixit.Core.DataContracts/FixPlans/Operations/Requests/FixPhaseStatusUpdateRequestDto.cs
@@ -1,12 +1,26 @@
-﻿using System.Runtime.Serialization;
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 using Fixit.Core.DataContracts.FixPlans.Phases.Enums;
+using Fixit.Core.DataContracts.Seeders;
 
 namespace Fixit.Core.DataContracts.FixPlans.Operations.Requests.FixPlans
 {
   [DataContract]
-  public class FixPhaseStatusUpdateRequestDto
+  public class FixPhaseStatusUpdateRequestDto : IFakeSeederAdapter<FixPhaseStatusUpdateRequestDto>
   {
     [DataMember]
     public PhaseStatuses Status { get; set; }
+
+    public IList<FixPhaseStatusUpdateRequestDto> SeedFakeDtos()
+    {
+      FixPhaseStatusUpdateRequestDto firstFixPhaseRequestDto = new FixPhaseStatusUpdateRequestDto
+      {
+        Status = PhaseStatuses.InProgress,
+      };
+
+      FixPhaseStatusUpdateRequestDto secondFixPhaseRequestDto = null;
+
+      return new List<FixPhaseStatusUpdateRequestDto> { firstFixPhaseRequestDto, secondFixPhaseRequestDto };
+    }
   }
 }
--- Fixit.Core.DataContracts/FixPlans/Operations/Requests/FixPlanUpdateRequestDto.cs
+++ Fixit.Core.DataContracts/FixPlans/Operations/Requests/FixPlanUpdateRequestDto.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 using Fixit.Core.DataContracts.FixPlans.Phases;
+using Fixit.Core.DataContracts.FixPlans.Phases.Tasks;
+using Fixit.Core.DataContracts.Seeders;
 
 namespace Fixit.Core.DataContracts.FixPlans.Operations.Requests.FixPlans
 {
   [DataContract]
-  public class FixPlanUpdateRequestDto
+  public class FixPlanUpdateRequestDto: IFakeSeederAdapter<FixPlanUpdateRequestDto>
   {
       [DataMember]
       public bool IsBookmarked { get; set; }
@@ -15,5 +18,34 @@ namespace Fixit.Core.DataContracts.FixPlans.Operations.Requests.FixPlans
 
       [DataMember]
       public long UpdatedTimestampUtc { get; set; }
+
+    public IList<FixPlanUpdateRequestDto> SeedFakeDtos()
+    {
+      FixPlanUpdateRequestDto firstFixPlan = new FixPlanUpdateRequestDto
+      {
+        Phases = new List<FixPhaseDto>()
+        {
+          new FixPhaseDto
+          {
+            Name = "Start",
+            Id = Guid.Parse("3e888ecb-0454-448a-90cd-6d16cc4d44eb"),
+            Tasks = new List<FixPhaseTaskDto>()
+            {
+              new FixPhaseTaskDto()
+              {
+                Description="Testing",
+                Id = Guid.Parse("1e811863-dea8-44c1-a95d-4b0b34a9970e"),
+                Name = "Test",
+                Order =0
+              }
+            }
+          }
+        }
+      };
+
+      FixPlanUpdateRequestDto secondFixPlan = null;
+
+      return new List<FixPlanUpdateRequestDto> { firstFixPlan, secondFixPlan };
+    }
   }
 }
