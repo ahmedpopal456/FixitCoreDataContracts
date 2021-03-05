68a201b926e35d7f1689d8cba16f45e8e2f4de08
Wei-Cheng Su <wc.su598@gmail.com>
2021-03-05 18:17:38 +0000

    Merged PR 214: skill dto
    Related work items: #512

---
+++ Fixit.Core.DataContracts/Users/Operations/Skills/UpdateUserSkillRequestDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Fixit.Core.DataContracts.Seeders;
+using Fixit.Core.DataContracts.Users.Skills;
+
+namespace Fixit.Core.DataContracts.Users.Operations
+{
+  public class UpdateUserSkillRequestDto : OperationStatus, IFakeSeederAdapter<UpdateUserSkillRequestDto>
+  {
+    [DataMember]
+    public Guid UserId { get; set; }
+
+    [DataMember]
+    public SkillDto Skill { get; set; }
+
+    [DataMember]
+    public long AttributedAtTimestampUtc { get; set; }
+
+    [DataMember]
+    public long ExpiresAtTimestampUtc { get; set; }
+
+    public IList<UpdateUserSkillRequestDto> SeedFakeDtos()
+    {
+      UpdateUserSkillRequestDto firstSkill = new UpdateUserSkillRequestDto
+      {
+        Skill = new SkillDto { Id = Guid.NewGuid(), Name = "Hello" },
+        AttributedAtTimestampUtc = DateTimeOffset.Now.ToUnixTimeSeconds(),
+        ExpiresAtTimestampUtc = DateTimeOffset.Now.ToUnixTimeSeconds()
+      };
+
+      UpdateUserSkillRequestDto secondSkill = null;
+
+      return new List<UpdateUserSkillRequestDto>
+      {
+        firstSkill,
+        secondSkill
+      };
+    }
+  }
+}
---
+++ Fixit.Core.DataContracts/Users/Skill/SkillDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Fixit.Core.DataContracts.Users.Skills
+{
+  public class SkillDto
+  {
+    [DataMember]
+    public Guid Id { get; set; }
+
+    [DataMember]
+    public string Name { get; set; }
+  }
+}
