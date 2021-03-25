bd63501bc81ff0852c27d5579042312dd765a9cf
Jack Leung <jack-_-leung@hotmail.com>
2021-03-25 22:06:37 +0000

    Merged PR 250: #559 added operation status for user summary
    #559 added operation status for user summary
    
    Related work items: #559

---
+++ Fixit.Core.DataContracts/Users/Operations/Profile/UserSummaryResponseDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Fixit.Core.DataContracts.Users.Enums;
+
+namespace Fixit.Core.DataContracts.Users.Operations.Profile
+{
+  [DataContract, KnownType(typeof(OperationStatus))]
+  public class UserSummaryResponseDto : OperationStatus
+  {
+    [DataMember]
+    public Guid Id { get; set; }
+
+    [DataMember]
+    public string FirstName { get; set; }
+
+    [DataMember]
+    public string LastName { get; set; }
+
+    [DataMember]
+    public string ProfilePictureUrl { get; set; }
+
+    [DataMember]
+    public UserRole Role { get; set; }
+
+    [DataMember]
+    public UserStatusDto Status { get; set; }
+  }
+}
