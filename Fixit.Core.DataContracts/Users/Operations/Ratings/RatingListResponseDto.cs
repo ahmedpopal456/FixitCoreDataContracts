88b6f83f621983b30d8759db72351ebeec63c532
David Chen <DavidChen_98@hotmail.com>
2021-03-29 19:48:30 +0000

    Merged PR 260: #535 added Id and EntityId into the response
    #535 added Id and EntityId into the response
    
    Related work items: #535

--- Fixit.Core.DataContracts/Users/Operations/Ratings/RatingListResponseDto.cs
+++ Fixit.Core.DataContracts/Users/Operations/Ratings/RatingListResponseDto.cs
@@ -1,10 +1,17 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
 
 namespace Fixit.Core.DataContracts.Users.Operations.Ratings
 {
     [DataContract]
     public class RatingListResponseDto 
     {
+        [DataMember]
+        public Guid Id { get; set; }
+
+        [DataMember]
+        public Guid EntityId { get; set; }
+
         [DataMember]
         public float AverageRating { get; set; }
 
