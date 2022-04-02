631292d4eddd07cc4748f38990d06fd86719fcc1
Ahmed Popal <ahmed@empower.social>
2022-04-02 17:12:16 -0400

    Added extra change

--- Fixit.Core.DataContracts/Chat/Operations/Messages/ChatMessageGroupSendMessage.cs
+++ Fixit.Core.DataContracts/Chat/Operations/Messages/ChatMessageGroupSendMessage.cs
@@ -5,16 +5,13 @@ using Fixit.Core.DataContracts.Chat.Operations.Requests;
 namespace Fixit.Core.DataContracts.Chat.Operations.Messages
 {
   [DataContract]
-  public class ChatMessageGroupSendMessage
+  public class ChatMessageGroupSendMessage : OnConversationAction
   {
     [DataMember]
     public Guid SentByUserId { get; set; }
 
     [DataMember]
     public long SentTimestampUtc { get; set; }
-
-    [DataMember]
-    public Guid ConversationId { get; set; }
 
     [DataMember]
     public ConversationMessageUpsertRequestDto MessageCreateRequest { get; set; }
